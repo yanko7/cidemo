f77cc224d9c4a4610c89441a641f67a32bb45c25
Yanko Bolanos <bolyanko@amazon.com>
2024-01-18 06:50:12 -0800

    added tests

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Greeter interface {
+	Greet() string
+}
+
+type englishGreet struct {
+}
+
+func (e *englishGreet) Greet() string {
+	return "hello\n"
+}
+
 func main() {
-	fmt.Printf("hello\n")
+	g := englishGreet{}
+	msg := g.Greet()
+	fmt.Printf(msg)
 }
